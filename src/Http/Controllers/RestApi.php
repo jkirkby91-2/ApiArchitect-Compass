67b858dde7632895d4528d99a40b27b15c607943
James Kirkby <jkirkby@protonmai.ch>
2017-02-04 17:24:21 +0800

    move getting requested user to resource controller

--- src/Http/Controllers/RestApi.php
+++ src/Http/Controllers/RestApi.php
@@ -2,6 +2,7 @@
 
 namespace ApiArchitect\Compass\Http\Controllers;
 
+use Tymon\JWTAuth\JWTAuth;
 use Jkirkby91\LumenRestServerComponent\Http\Controllers\ResourceController;
 use Jkirkby91\Boilers\RestServerBoiler\TransformerContract as ObjectTransformer;
 use Jkirkby91\Boilers\RepositoryBoiler\ResourceRepositoryContract as ResourceRepository;
@@ -14,19 +15,8 @@ use Jkirkby91\Boilers\RepositoryBoiler\ResourceRepositoryContract as ResourceRep
 abstract class RestApi extends ResourceController
 {
 
-    protected $user;
-
     public function __construct(ResourceRepository $repository, ObjectTransformer $objectTransformer)
     {
         parent::__construct($repository,$objectTransformer);
-        $this->user = app()->make('auth')->getUser();
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getUser()
-    {
-        return $this->user;
     }
 }
