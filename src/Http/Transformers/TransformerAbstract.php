7c6bb263526ff337081a3fcb7252faa441573c89
James Kirkby <jkirkby@protonmail.ch>
2017-05-10 01:05:07 +0800

    update abstract to match contract

--- src/Http/Transformers/TransformerAbstract.php
+++ src/Http/Transformers/TransformerAbstract.php
@@ -17,7 +17,7 @@ abstract class TransformerAbstract extends SuperAbstract
      * @param $object
      * @return static
      */
-    abstract protected function doTransform($object);
+    abstract protected function transform($object);
 
     /**
      * Wraps a endpoint response object, into a standardised format
