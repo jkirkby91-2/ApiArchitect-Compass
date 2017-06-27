81904df993dba9895ef3955dabf7c6e388a0d695
James Kirkby <jkirkby@protonmail.ch>
2017-06-28 01:50:36 +0800

    refacor api controller to pass auth user to it

---
+++ src/Http/Controllers/ResourceApi.php
@@ -0,0 +1,66 @@
+<?php
+
+	namespace ApiArchitect\Compass\Http\Controllers;
+
+	use Tymon\JWTAuth\JWTAuth;
+	use Jkirkby91\LumenRestServerComponent\Http\Controllers\ResourceController;
+	use Jkirkby91\Boilers\RestServerBoiler\TransformerContract as ObjectTransformer;
+	use Jkirkby91\Boilers\RepositoryBoiler\ResourceRepositoryContract as ResourceRepository;
+
+	/**
+	 * Class ResourceApi
+	 *
+	 * @package ApiArchitect\Compass\Http\Controllers
+	 * @author  James Kirkby <jkirkby@protonmail.ch>
+	 */
+	abstract class ResourceApi extends ResourceController
+	{
+		
+		protected $user;
+
+		/**
+		 * RestApi constructor.
+		 *
+		 * @param \Jkirkby91\Boilers\RepositoryBoiler\ResourceRepositoryContract $repository
+		 * @param \Jkirkby91\Boilers\RestServerBoiler\TransformerContract        $objectTransformer
+		 */
+		public function __construct(ResourceRepository $repository, ObjectTransformer $objectTransformer)
+		{
+			parent::__construct($repository,$objectTransformer);
+		}
+
+		/**
+		 * validateRequestedUserContentPath()
+		 * @param $uid
+		 */
+		public function validateRequestedUserContentPath($uid)
+		{
+			if ($uid != $this->user->getId())
+			{
+				throw new UnauthorizedHttpException;
+			}
+		}
+
+		/**
+		 * getRequestUser()
+		 * @return mixed
+		 */
+		public function getUser()
+		{
+			return $this->user;
+		}
+
+		/**
+		 * setUser()
+		 * @param $user
+		 *
+		 * @return $this
+		 */
+		public function setUser($user)
+		{
+			$this->user = $user;
+			return $this;
+		}
+	
+	}
+	
