3486c65df55457e77df3dd8fb336adec5c6fc7dc
James Kirkby <jkirkby@protonmail.ch>
2017-10-20 16:47:53 +0200

    update formatting

--- src/Http/Middleware/AbstractMiddleware.php
+++ src/Http/Middleware/AbstractMiddleware.php
@@ -1,18 +1,26 @@
 <?php
 
-namespace ApiArchitect\Compass\Http\Middleware;
+	namespace ApiArchitect\Compass\Http\Middleware {
 
-use Closure;
-use Psr\Http\Message\ServerRequestInterface;
+		use Closure;
+		use Psr\Http\Message\ServerRequestInterface;
 
-abstract class AbstractMiddleware
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    abstract public function handle(ServerRequestInterface $request, Closure $next);
-}
+		/**
+		 * Class AbstractMiddleware
+		 *
+		 * @package ApiArchitect\Compass\Http\Middleware
+		 * @author  James Kirkby <jkirkby@protonmail.ch>
+		 */
+		abstract class AbstractMiddleware
+		{
+
+			/**
+			 * handle()
+			 * @param \Psr\Http\Message\ServerRequestInterface $request
+			 * @param \Closure                                 $next
+			 *
+			 * @return mixed
+			 */
+			abstract public function handle(ServerRequestInterface $request, Closure $next);
+		}
+	}
